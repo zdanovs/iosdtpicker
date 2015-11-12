3ed03d4083b73b959fa6161b5fd80d6217ed2f9c
okertanov <okertanov@gmail.com>
2015-11-12 21:14:48 +0200

    Initial CTDateTimePicker library

---
+++ CTDateTimePicker/CTDateTimePicker/CTDateTimePicker.h
@@ -0,0 +1,13 @@
+//
+//  CTDateTimePicker.h
+//  CTDateTimePicker
+//
+//  Created by Oleg Kertanov on 12/11/15.
+//  Copyright © 2015 Oleg Kertanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTDateTimePicker : NSObject
+
+@end
---
+++ CTDateTimePicker/CTDateTimePicker/CTDateTimePicker.m
@@ -0,0 +1,13 @@
+//
+//  CTDateTimePicker.m
+//  CTDateTimePicker
+//
+//  Created by Oleg Kertanov on 12/11/15.
+//  Copyright © 2015 Oleg Kertanov. All rights reserved.
+//
+
+#import "CTDateTimePicker.h"
+
+@implementation CTDateTimePicker
+
+@end
